beac8c11bac76f14452dc9015bb2187c944103be
Jesper Pedersen <jeppenp12@gmail.com>
2018-12-14 04:17:33 +0100

    Add Favicon

--- wp-content/themes/jespernpedersen/header.php
+++ wp-content/themes/jespernpedersen/header.php
@@ -16,6 +16,23 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js"></script>
 	<?php wp_head(); ?>
+	<link rel="apple-touch-icon" sizes="57x57" href="/portfolio-v3/wp-content/themes/jespernpedersen/assets/images/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/portfolio-v3/wp-content/themes/jespernpedersen/assets/images/favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/portfolio-v3/wp-content/themes/jespernpedersen/assets/images/favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/portfolio-v3/wp-content/themes/jespernpedersen/assets/images/favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/portfolio-v3/wp-content/themes/jespernpedersen/assets/images/favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/portfolio-v3/wp-content/themes/jespernpedersen/assets/images/favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/portfolio-v3/wp-content/themes/jespernpedersen/assets/images/favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/portfolio-v3/wp-content/themes/jespernpedersen/assets/images/favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="/portfolio-v3/wp-content/themes/jespernpedersen/assets/images/favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/portfolio-v3/wp-content/themes/jespernpedersen/assets/images/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/portfolio-v3/wp-content/themes/jespernpedersen/assets/images/favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/portfolio-v3/wp-content/themes/jespernpedersen/assets/images/favicon/favicon-16x16.png">
+	<link rel="manifest" href="/portfolio-v3/wp-content/themes/jespernpedersen/assets/images/favicon/manifest.json">
+	<meta name="msapplication-TileColor" content="#4d94bf">
+	<meta name="msapplication-TileImage" content="/portfolio-v3/wp-content/themes/jespernpedersen/assets/images/favicon/ms-icon-144x144.png">
+	<meta name="theme-color" content="#4d94bf">
 </head>
 
 <body class="not-active <?php 
