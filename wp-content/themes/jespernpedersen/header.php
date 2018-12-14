9591c5ec1aea9212decc201a523c485f7fa76be3
Jesper Pedersen <jeppenp12@gmail.com>
2018-12-14 12:47:41 +0100

    Add Google Analystics

--- wp-content/themes/jespernpedersen/header.php
+++ wp-content/themes/jespernpedersen/header.php
@@ -7,7 +7,17 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
+	<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-131042303-1"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-131042303-1');
+	</script>
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
